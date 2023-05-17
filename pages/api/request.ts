f9d682f6396f7858885431b761e9128aec1c5406
zenchantlive <jordanlive121@gmail.com>
2023-05-16 18:38:23 -0700

    commit

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import server from "../../../AbletonerwLogin/config-server";
+import server from "../../config-server";
 import { NextRequest } from "next/server";
 import { OpenAIStream } from "../../helpers/openai-stream";
 
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 };
 
 export default async function handler(req: NextRequest) {
