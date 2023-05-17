313b86803f864e882d1b81566548b44a546bb328
zenchantlive <jordanlive121@gmail.com>
2023-05-16 21:37:45 -0700

    commit

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Preahvihear, Space_Grotesk } from "next/font/google";
+import { Preahvihear, Space_Grotesk } from "@next/font/google";
 import classNames from "classnames";
 import BackgroundGradient from "../components/background-gradient";
 import Card from "../components/card";
