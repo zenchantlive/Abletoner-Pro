dfeb5f6e51cd8d4ab09824d9c68bd94fb5b5ef44
zenchantlive <jordanlive121@gmail.com>
2023-05-16 21:10:37 -0700

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
-
+import '../styles/global.module.css';
+import '../styles/button.module.css';
 type CustomSession = {
   id: string;
   email: string;
   name: string;
   session: any;
 };
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   const session = useSession();
