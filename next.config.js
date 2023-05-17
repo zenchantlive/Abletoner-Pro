709458be74c4888e11bb86a3e3666f81a0a56bf3
zenchantlive <jordanlive121@gmail.com>
2023-05-16 18:31:41 -0700

    commit

--- next.config.js
+++ next.config.js
@@ -9,4 +9,20 @@ const nextConfig = {
   },
 };
 
+module.exports = {
+  ...nextConfig,
+  env: {
+    // Export envs to browser side
+    appName: process.env.APP_NAME,
+    appLogo: process.env.APP_LOGO,
+    appSummary: process.env.APP_SUMMARY,
+    appThemeColor: process.env.APP_THEME_COLOR,
+    exampleInput: process.env.EXAMPLE_INPUT,
+  },
+  httpAgentOptions: {
+    keepAlive: true,
+  },
+};
+
+
 module.exports = nextConfig;
---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+type CustomSession = {
+  id: string;
+  email: string;
+  name: string;
+  session: any;
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const session = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      return; // Do nothing if the user is logged in
+    }
+    router.push('/app/login/page'); // Redirect to login if not authenticated
+  }, [session, router]); // Include session in the dependency array
+
+
+  // Render the component only when the user is logged in
+  if (session) {
+    return <Component {...pageProps} />;
+  }
+
+  // Optional loading state
+  return <div>Loading...</div>;
+}
+
+export default MyApp;
